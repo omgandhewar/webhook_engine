af1d21a463af386e2f7711ada116455115ff7314
omgandhewar <gandhewarom@gmail.com>
2026-09-15 23:24:32 +0530

    add request retry

--- app/db/database.py
+++ app/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL="mysql+pymysql://root:root@localhost/webhook_engine.db"
+DATABASE_URL="mysql+pymysql://root:root@localhost/webhook_engine"
 
 
 engine=create_engine(
--- app/services/webhook_service.py
+++ app/services/webhook_service.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request, HTTPException
 from sqlalchemy import text
+import requests
+import json
 from urllib.parse import urlparse
 
 
@@ -9,7 +11,7 @@ def user_webhook(user,db):
     event=user.event
     url=user.target_url
     payload=user.payload
-    
+    payload1=json.dumps(user.payload)
     parsedurl=urlparse(url)
     
     print(parsedurl)
@@ -17,16 +19,46 @@ def user_webhook(user,db):
     if not parsedurl.scheme and not parsedurl.netloc:
         raise HTTPException(status_code=422,detail="invalid url")
     
-    db.execute(
-        text("INSERT INTO webhook(event,target_url,payload) VALUES(:event,:target_url,:payload)"),
+    result=db.execute(
+        text("INSERT INTO wehbook(event,target_url,payload) VALUES(:event,:target_url,:payload)"),
         {
             "event":event,
             "target_url":url,
-            "payload":payload
+            "payload":payload1
         }
     )
         
     db.commit()
+    
+    print(payload)
+    print(type(payload))
+    print(result)
+    
+    response=requests.post(url,json=payload)
+    
+    if response.status_code!=200:
+        
+        for i in range(0,3):
+            response1=requests.post(url,json=payload)
+            
+            if response1.status_code==200:
+                result1=db.execute(
+                            text("Update TABLE wehbook SET status=:status WHERE id=:id"),
+                            {
+                                "status":"Success"
+                                "id":wehbbok_id
+                            }
+                        )
+                break
+        if response1.status_code!=200:  
+              result1=db.execute(
+                        text("Update TABLE wehbook SET status=:status WHERE id=:id"),
+                        {
+                            "status":"failed"
+                            "id":wehbbok_id
+                        }
+                    )
+      
     
     return{
         "message":"webhook created successfully"
